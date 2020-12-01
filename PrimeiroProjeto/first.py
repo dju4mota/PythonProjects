cf83065cee560bb93a54912463dd182d68c44cb3
dju4mota <fernando.b@ges.inatel.com>
2020-12-01 13:51:28 -0300

    first implementation

--- PrimeiroProjeto/first.py
+++ PrimeiroProjeto/first.py
@@ -1,3 +1,17 @@
-word = input("Enter a word: ")
-print(word)
-print("Searching ...")
+import json
+import time
+
+data = json.load(open("data.json"))
+
+
+def search(word):
+    return data[word]
+
+
+print("Starting ...")
+time.sleep(2)
+
+results = search(input("Enter a word: "))
+
+for meaning in results:
+    print(meaning)
